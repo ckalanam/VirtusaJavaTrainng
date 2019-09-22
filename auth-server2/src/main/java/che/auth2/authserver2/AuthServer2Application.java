f3dadcfe1b5d2c1ca5ed104e667a4e853c2e4cc4
ckalanam <54342954+ckalanam@users.noreply.github.com>
2019-09-23 04:33:03 +0530

    from home
    auth-new

---
+++ auth-server2/src/main/java/che/auth2/authserver2/AuthServer2Application.java
@@ -0,0 +1,13 @@
+package che.auth2.authserver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServer2Application.class, args);
+	}
+
+}
